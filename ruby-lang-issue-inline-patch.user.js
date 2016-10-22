0721d5e9e6fc4057bd43dacb2d7bab3c7dcf9ec3
Code Ass <aycabta@gmail.com>
2016-10-23 03:10:42 +0900

    Add @description metadata

--- ruby-lang-issue-inline-patch.user.js
+++ ruby-lang-issue-inline-patch.user.js
@@ -2,6 +2,7 @@
 // @name       bugs.ruby-lang.org issue inline patch
 // @namespace  http://aycabta.github.io/
 // @version    0.1.4
+// @description  Names and natures often agree.
 // @include    /^https:\/\/bugs\.ruby-lang\.org\/issues\/\d+/
 // @copyright  2016+, Code Ass
 // ==/UserScript==
